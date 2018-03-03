7ee32d72b4c8956f5763e6da86d7be9590530e5a
Torne Wuff <torne@wolfpuppy.org.uk>
2018-03-03 16:52:42 -0500

    Set up mod name and description

--- src/main/java/uk/org/wolfpuppy/minecraft/chunkchecker/ChunkChecker.java
+++ src/main/java/uk/org/wolfpuppy/minecraft/chunkchecker/ChunkChecker.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package uk.org.wolfpuppy.minecraft.chunkchecker;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,13 +7,9 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = "chunkchecker", useMetadata = true)
+public class ChunkChecker
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
-
     private static Logger logger;
 
     @EventHandler
